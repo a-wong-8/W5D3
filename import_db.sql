9bbf70b51cb1ec3ab91e01ad3d60a7a63b663d43
Your Name <129026648+Vneet11@users.noreply.github.com>
2023-04-26 12:21:09 -0700

    import_db.sql almost

--- import_db.sql
+++ import_db.sql
@@ -1,6 +1,11 @@
+PRAGMA foreign_keys = ON;
+
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS questions;
-DROP TABLE IF EXISTS questions_follows;
+DROP TABLE IF EXISTS questions_follows
+DROP TABLE IF EXISTS replies ;
+DROP TABLE IF EXISTS question_likes;
+
 
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
@@ -21,3 +26,41 @@ CREATE TABLE users (
     user_id INTEGER
     questions_id INTEGER
  );
+
+ CREATE TABLE replies (
+   id INTEGER PRIMARY KEY,
+   body VARCHAR(255) NOT NULL,
+   question_id INTEGER NOT NULL, 
+   user_id INTEGER NOT NULL,
+   parent_reply_id INTEGER,
+
+   FOREIGN KEY (question_id) REFERENCES questions(id)
+
+   FOREIGN KEY (user_id) REFERENCES users(id)
+
+   FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
+
+ )
+
+ CREATE TABLE question_likes (
+   id INTEGER PRIMARY KEY, 
+   user_id INTEGER NOT NULL,
+   question_id INTEGER NOT NULL, 
+   number_of_likes INTEGER NOT NULL,
+
+   FOREIGN KEY (question_id) REFERENCES questions(id)
+
+   FOREIGN KEY (user_id) REFERENCES users(id)
+ )
+
+
+INSERT INTO 
+   users (fname, lname)
+VALUES
+   ('Arthur', 'Miller'), 
+   ('Eugene', 'Neil')
+
+INSERT INTO 
+   questions 
+VALUES   
+ ('Q1', 'What is your first name?', author_id)
